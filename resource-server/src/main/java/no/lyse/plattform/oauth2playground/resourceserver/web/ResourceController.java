e704a7d0e507783224fed83383d50aa185fdc0c9
Martin Goldhahn <mgoldhahn@gmail.com>
2023-03-06 23:07:57 +0100

    fix deserialization

--- resource-server/src/main/java/no/lyse/plattform/oauth2playground/resourceserver/web/ResourceController.java
+++ resource-server/src/main/java/no/lyse/plattform/oauth2playground/resourceserver/web/ResourceController.java
@@ -7,8 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.text.MessageFormat;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +19,11 @@ public class ResourceController {
     private final HttpExchangeRepository  traces;
 
     @GetMapping(value = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<String> logMessages() {
-        return Flux.just(
+    public Mono<String[]> logMessages() {
+        return Mono.just(List.of(
             "Artificial Intelligence is no match for natural stupidity.",
             "The time to relax is when you don't have time for it."
-        );
+        ).toArray(new String[0]));
 //        return Flux.fromStream(traces.findAll().stream())
 //            .map(exc -> MessageFormat.format("{0}: ", exc.getTimestamp(), exc.getRequest().getUri()));
     }
