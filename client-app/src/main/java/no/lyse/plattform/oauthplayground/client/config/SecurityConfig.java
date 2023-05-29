4bf327bdfff6815f0be0db28aa535fe94c46cbaa
Martin Goldhahn <mgoldhahn@gmail.com>
2023-05-29 15:50:37 +0200

    upgrade deprecated methods

--- client-app/src/main/java/no/lyse/plattform/oauthplayground/client/config/SecurityConfig.java
+++ client-app/src/main/java/no/lyse/plattform/oauthplayground/client/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
             .logout(logout ->
                 logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
             )
-            .csrf().disable()
+            .csrf(csrf -> csrf.disable())
             .build();
     }
     // @formatter:on
